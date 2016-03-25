f89c140444992bccb45c35489a59011b61d6cb61
drydd <denisweil@gmx.de>
2016-03-25 16:17:29 +0100

    Finished Tutorial Part 1

---
+++ Assets/Voxel Engine/BlockAir.cs
@@ -0,0 +1,24 @@
+﻿public class BlockAir : Block
+{
+    #region Public Constructors
+
+    public BlockAir() : base()
+    {
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    public override MeshData Blockdata(Chunk chunk, int x, int y, int z, MeshData meshData)
+    {
+        return meshData;
+    }
+
+    public override bool IsSolid(Direction direction)
+    {
+        return false;
+    }
+
+    #endregion Public Methods
+}
---
+++ Assets/Voxel Engine/MeshData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData
+{
+    #region Public Fields
+
+    public List<int> colTriangles = new List<int>();
+    public List<Vector3> colVertices = new List<Vector3>();
+    public List<int> triangles = new List<int>();
+    public List<Vector2> uv = new List<Vector2>();
+    public List<Vector3> vertices = new List<Vector3>();
+
+    #endregion Public Fields
+
+    #region Public Constructors
+
+    public MeshData()
+    {
+    }
+
+    #endregion Public Constructors
+
+    #region Internal Methods
+
+    internal void AddQuadTriangles()
+    {
+        triangles.Add(vertices.Count - 4);
+        triangles.Add(vertices.Count - 3);
+        triangles.Add(vertices.Count - 2);
+
+        triangles.Add(vertices.Count - 4);
+        triangles.Add(vertices.Count - 2);
+        triangles.Add(vertices.Count - 1);
+    }
+
+    #endregion Internal Methods
+}
